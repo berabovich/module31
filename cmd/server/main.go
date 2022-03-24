826a8e32c5d52a0bb485e2208028be1f28114654
Kirill Semerenko <semerenkok@rambler.ru>
2022-03-24 10:46:42 +0300

    aaa

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"log"
 	"module31/internal/app"
+	"os"
 )
 
 func main() {
-	//port := os.Args
-	port := []string{"", ":8080"}
+	port := os.Args
+	//port := []string{"", ":8080"}
 	err := app.Run(port[1])
 	if err != nil {
 		log.Fatal(err)
