1d7df428cab23eb90d95d1e0eed1c236cdfee9f7
Kirill Semerenko <semerenkok@rambler.ru>
2022-03-29 11:06:36 +0300

    aaa

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,14 +1,23 @@
 package app
 
 import (
+	"context"
+	"fmt"
 	"github.com/go-chi/chi/v5"
+	"log"
 	"module31/internal/controller"
 	"module31/internal/repository"
 	"module31/internal/usecase"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func Run(port string) error {
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	//repositoryA, err := repository.NewMemorydb()
 	repositoryA, err := repository.NewMongodb()
 	if err != nil {
@@ -17,7 +26,16 @@ func Run(port string) error {
 	useCase := usecase.NewUsecase(repositoryA)
 	router := chi.NewRouter()
 	controller.Build(router, useCase)
+	go func() {
+		log.Fatal(http.ListenAndServe(port, router))
+	}()
+	<-done
+	_, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
 
+		fmt.Println("App close")
+		cancel()
+	}()
 	err = http.ListenAndServe(port, router)
 	if err != nil {
 		return err
